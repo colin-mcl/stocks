4b13ed54cd8e9296cf8985c6fb99473ede56f7df
colin-mcl <colin.mclaughlin02@gmail.com>
2024-04-23 10:46:53 -0400

    Add basic handler and print hello world

--- server/server.go
+++ server/server.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+)
 
+// Handler function for "/"
+// Accepts an http ResponseWriter which is used to write info back to the client
+// and a *http.Request which is used to et info about the request
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello world!")
+}
+
+func main() {
+	// Sets up the handler function for a SPECIFIC request path
+	http.HandleFunc("/", getRoot)
+
+	// Tells global HTTP server to listen for incoming requests on port
+	err := http.ListenAndServe(":8080", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server %s\n", err)
+		os.Exit(1)
+	}
 }
