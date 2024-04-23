60a7f4f278118780e53ab1fbfca65af1e2c8efb5
colin-mcl <colin.mclaughlin02@gmail.com>
2024-04-23 13:40:19 -0400

    Add basic read loop

--- CLI/reader.go
+++ CLI/reader.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 /* reader.go
@@ -25,9 +27,22 @@ Usage is as follows:
 */
 
 func main() {
-	// reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Printf("\t\t\t STOCKS PROGRAM\n")
-	fmt.Printf("\tPlease enter 'get' followed by the stock ticker you would like to retrieve\n")
+	fmt.Printf("\t\t\t\t\t  STOCKS PROGRAM\n")
+	fmt.Printf("Please enter 'get' followed by the stock ticker you would like to retrieve, or enter 'q' to quit\n")
+	fmt.Println("-----------------------------------------------------------------------------------------------")
+
+	for {
+		fmt.Print("-> ")
+		text, err := reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println(text)
+	}
 
 }
