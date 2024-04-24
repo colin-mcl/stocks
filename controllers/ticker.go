f51dfe795dc5cdcc0836e59a25e69657f168b029
colin-mcl <colin.mclaughlin02@gmail.com>
2024-04-24 16:09:10 -0400

    Change to Gin

---
+++ controllers/ticker.go
@@ -0,0 +1,11 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+const (
+	yahooURL = "https://query1.finance.yahoo.com/v8/finance/chart/%s"
+)
+
+func GetTicker(c *gin.Context) {
+	return
+}
