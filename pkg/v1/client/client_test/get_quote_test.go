044ab00d911de222350bf246f0e6ad3fcced4e0f
colin-mcl <colin.mclaughlin02@gmail.com>
2024-08-25 15:35:07 -0400

    Implement GetQuote test on client

--- pkg/v1/client/client_test/get_quote_test.go
+++ pkg/v1/client/client_test/get_quote_test.go
@@ -1,8 +1,11 @@
 package client_test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/colin-mcl/stocks/util"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -11,4 +14,17 @@ func TestGetQuote(t *testing.T) {
 	assert.Error(t, err)
 	assert.ErrorContains(t, err, "invalid quote symbol")
 	assert.Equal(t, s, "")
+
+	r := strings.ToUpper(util.RandomString(20))
+	s, err = testClient.GetQuote(r)
+	assert.Error(t, err)
+	assert.ErrorContains(t, err, fmt.Sprintf("invalid quote symbol: %s", r))
+	assert.Equal(t, s, "")
+
+	s, err = testClient.GetQuote("tsla")
+	assert.NoError(t, err)
+	assert.NotEqual(t, s, "")
+	assert.Contains(t, s, "TSLA")
+	assert.Contains(t, s, "Tesla")
+	assert.Contains(t, s, "USD")
 }
